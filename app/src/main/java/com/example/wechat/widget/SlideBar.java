e44d933c0f8d3635f887b64a4cab032a34f4eab0
salmonzhang <ahzhangxuping@163.com>
2017-08-17 00:31:41 +0800

    计算点中的字母

--- app/src/main/java/com/example/wechat/widget/SlideBar.java
+++ app/src/main/java/com/example/wechat/widget/SlideBar.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.wechat.R;
+import com.example.wechat.Utils.ToastUtil;
 import com.hyphenate.util.DensityUtil;
 
 /**
@@ -19,18 +20,18 @@ import com.hyphenate.util.DensityUtil;
  */
 
 public class SlideBar extends View {
-    private static String[] SECTIONS = {"搜","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    private static String[] SECTIONS = {"搜", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     private int mHeight;
     private int mWidth;
     private float mAverHeight;
     private Paint mPaint;
 
     public SlideBar(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public SlideBar(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public SlideBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -42,7 +43,7 @@ public class SlideBar extends View {
         //初始化一个画笔
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);//抗锯齿
         mPaint.setTextAlign(Paint.Align.CENTER);
-        mPaint.setTextSize(DensityUtil.dip2px(getContext(),12));
+        mPaint.setTextSize(DensityUtil.dip2px(getContext(), 12));
         mPaint.setColor(0xff9c9c9c);
 
     }
@@ -54,7 +55,7 @@ public class SlideBar extends View {
         mHeight = getMeasuredHeight();
         mWidth = getMeasuredWidth();
         //获取平均高度
-        mAverHeight = (mHeight+0.0f )/ SECTIONS.length;
+        mAverHeight = (mHeight + 0.0f) / SECTIONS.length;
     }
 
     @Override
@@ -62,8 +63,8 @@ public class SlideBar extends View {
         super.onDraw(canvas);
         for (int i = 0; i < SECTIONS.length; i++) {
             float X = mWidth / 2;
-            float Y = mAverHeight *(i + 0.6f);
-            canvas.drawText(SECTIONS[i],X,Y,mPaint);
+            float Y = mAverHeight * (i + 0.6f);
+            canvas.drawText(SECTIONS[i], X, Y, mPaint);
         }
     }
 
@@ -78,6 +79,8 @@ public class SlideBar extends View {
                  * 3：当选中的字母与通讯录中的字母一致时，让RecyclerView滚动
                  */
                 setBackgroundResource(R.drawable.shape_slidebar_bg);
+                float y = event.getY();
+                showFloatViewAndScrollRecyclerView(y);
                 break;
             case MotionEvent.ACTION_UP:
                 /**
@@ -88,5 +91,18 @@ public class SlideBar extends View {
                 break;
         }
         return true;
+    }
+
+    //显示FloatView并滚动RecyclerView
+    private void showFloatViewAndScrollRecyclerView(float y) {
+        //通过手指点击的位置，获取当前索引
+        int position = (int)(y / mAverHeight);
+        if (position < 0) {
+            position = 0;
+        }
+        if (position > SECTIONS.length - 1) {
+            position = SECTIONS.length - 1;
+        }
+        ToastUtil.showToast(SECTIONS[position]);
     }
 }

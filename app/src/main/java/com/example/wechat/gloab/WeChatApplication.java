3c0201d27445ece59968e75bdb6e03d15312d62e
salmonzhang <ahzhangxuping@163.com>
2017-08-17 17:30:14 +0800

    接收好友邀请

--- app/src/main/java/com/example/wechat/gloab/WeChatApplication.java
+++ app/src/main/java/com/example/wechat/gloab/WeChatApplication.java
@@ -8,10 +8,12 @@ import android.os.Handler;
 import android.util.Log;
 
 import com.avos.avoscloud.AVOSCloud;
+import com.example.wechat.Utils.ThreadFactory;
 import com.example.wechat.event.ContactUpdateEvent;
 import com.hyphenate.EMContactListener;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMOptions;
+import com.hyphenate.exceptions.HyphenateException;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -63,9 +65,24 @@ public class WeChatApplication extends Application {
             }
 
             @Override
-            public void onContactInvited(String username, String reason) {
+            public void onContactInvited(final String username, String reason) {
                 //收到好友邀请
-                Log.d(TAG, "onContactInvited: ");
+                Log.d(TAG, "onContactInvited: "+"username = " + username +"reason = "+ reason);
+                ThreadFactory.runOnSubThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ThreadFactory.runOnSubThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    EMClient.getInstance().contactManager().acceptInvitation(username);
+                                } catch (HyphenateException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
             }
 
             @Override
@@ -91,14 +108,14 @@ public class WeChatApplication extends Application {
 
     private void initHuanXin() {
         EMOptions options = new EMOptions();
-    // 默认添加好友时，是不需要验证的，改成需要验证
+        // 默认添加好友时，是不需要验证的，改成需要验证
         options.setAcceptInvitationAlways(false);
 
         int pid = android.os.Process.myPid();
         String processAppName = getAppName(pid);
-    // 如果APP启用了远程的service，此application:onCreate会被调用2次
-    // 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
-    // 默认的APP会在以包名为默认的process name下运行，如果查到的process name不是APP的process name就立即返回
+        // 如果APP启用了远程的service，此application:onCreate会被调用2次
+        // 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
+        // 默认的APP会在以包名为默认的process name下运行，如果查到的process name不是APP的process name就立即返回
 
         if (processAppName == null ||!processAppName.equalsIgnoreCase(getPackageName())) {
             Log.e(TAG, "enter the service process!");
